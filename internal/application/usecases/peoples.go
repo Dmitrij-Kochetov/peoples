77858ee173b1a5698701239001a0f02e527f0f9a
dmitrij <cwfrcpp@gmail.com>
2023-09-13 19:53:56 +0300

    peoples repo removed from domain, moved to usecases

--- internal/application/usecases/peoples.go
+++ internal/application/usecases/peoples.go
@@ -1,1 +1,15 @@
 package usecases
+
+import (
+	"context"
+	"github.com/Dmitrij-Kochetov/peoples/internal/domain/dto"
+	"github.com/google/uuid"
+)
+
+type IPeopleRepo interface {
+	GetByID(context.Context, uuid.UUID) (dto.People, error)
+	GetAllByFilter(context.Context, dto.Filter) (dto.Peoples, error)
+	Create(context.Context, dto.CreatePeople) (uuid.UUID, error)
+	Update(context.Context, dto.People) error
+	DeleteByID(context.Context, uuid.UUID) error
+}
