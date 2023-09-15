afe03da0d3936be8a525e390f553adae5ef32bae
dmitrij <cwfrcpp@gmail.com>
2023-09-15 22:03:30 +0300

    updated reading config for kafka service

--- internal/adapter/config/kafka_config/config.go
+++ internal/adapter/config/kafka_config/config.go
@@ -1,5 +1,20 @@
 package kafka_config
 
 type Config struct {
-	Data string `env:"DATA"`
+	Env   string `env:"ENV"`
+	Kafka KafkaConfig
+	DB    DbConfig
+}
+
+type KafkaConfig struct {
+	KafkaURL      string `env:"KAFKA_URL"`
+	ConsumerTopic string `env:"KAFKA_CONSUMER_TOPIC"`
+	ConsumerGroup string `env:"KAFKA_CONSUMER_GROUP"`
+	ProducerTopic string `env:"KAFKA_PRODUCER_TOPIC"`
+	KafkaTimeout  int    `env:"KAFKA_TIMEOUT"`
+}
+
+type DbConfig struct {
+	Driver string `env:"DB_DRIVER"`
+	DbURL  string `env:"DB_URL"`
 }
