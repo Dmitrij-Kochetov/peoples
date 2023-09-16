ec9757f30f5de405c31f051d60be49f2a7ad3c0c
dmitrij <cwfrcpp@gmail.com>
2023-09-16 11:18:26 +0300

    Updated config for rest service

--- internal/adapter/config/rest_config/config.go
+++ internal/adapter/config/rest_config/config.go
@@ -1,5 +1,28 @@
 package rest_config
 
+import "time"
+
 type Config struct {
-	DB string `env:"DB"`
+	Env    string `env:"ENV"`
+	Db     DbConfig
+	Server ServerConfig
+	Redis  RedisConfig
+}
+
+type DbConfig struct {
+	Driver string `env:"DB_DRIVER"`
+	Url    string `env:"DB_URL"`
+}
+
+type ServerConfig struct {
+	Address     string        `env:"SERVER_ADDRESS"`
+	Timeout     time.Duration `env:"SERVER_TIMEOUT"`
+	IdleTimeout time.Duration `env:"SERVER_IDLE_TIMEOUT"`
+}
+
+type RedisConfig struct {
+	Address  string        `env:"REDIS_ADDRESS"`
+	Password string        `env:"REDIS_PASSWORD"`
+	DB       int           `env:"REDIS_DB"`
+	Timeout  time.Duration `env:"REDIS_PING_TIMEOUT"`
 }
