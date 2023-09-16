30cd7663dee26455fbd11496e163abeb17b31d0d
dmitrij <cwfrcpp@gmail.com>
2023-09-16 11:18:15 +0300

    Created server struct and main for rest service

--- cmd/peoples_rest/main.go
+++ cmd/peoples_rest/main.go
@@ -1,13 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config"
 	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config/rest_config"
+	"github.com/Dmitrij-Kochetov/peoples/internal/application/presentation/rest"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	var cfg rest_config.Config
 	cfg = config.LoadConfig(cfg)
-	fmt.Printf("%v\n", cfg)
+	errChan, err := run(cfg)
+	if err != nil {
+		log.Fatalf("Couldn't run: %v", err)
+	}
+	if err := <-errChan; err != nil {
+		log.Fatalf("Error while running: %v", err)
+	}
+}
+
+func run(cfg rest_config.Config) (<-chan error, error) {
+	server, err := rest.NewServerFromConfig(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	errChan := make(chan error, 1)
+
+	ctx, stop := signal.NotifyContext(context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+		syscall.SIGQUIT,
+	)
+
+	go func() {
+		<-ctx.Done()
+		log.Println("Shutting down...")
+
+		ctxTimeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+		defer func() {
+			redisCtxTimeout, c := context.WithTimeout(ctxTimeout, 5*time.Second)
+			err := server.Close(redisCtxTimeout)
+			if err != nil {
+				log.Fatalf("Couldn't close server: %v", err)
+			}
+			stop()
+			cancel()
+			c()
+			close(errChan)
+		}()
+
+		if err := server.Shutdown(ctxTimeout); err != nil {
+			errChan <- err
+		}
+
+		log.Println("Gracefully shutting down")
+	}()
+
+	go func() {
+		log.Println("Starting server...")
+
+		if err := server.ListenAndServe(); err != nil {
+			errChan <- err
+		}
+	}()
+
+	return errChan, nil
 }
---
+++ internal/application/presentation/rest/server.go
@@ -0,0 +1,77 @@
+package rest
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-chi/chi"
+	"log/slog"
+
+	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config/rest_config"
+	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/logging"
+	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/repo"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
+)
+
+type Server struct {
+	logger    *slog.Logger
+	repo      *repo.PeopleRepo
+	router    *chi.Mux
+	doneChan  chan struct{}
+	closeChan chan struct{}
+}
+
+func NewServerFromConfig(cfg rest_config.Config) (*Server, error) {
+	logger := logging.SetUpLogger(cfg.Env)
+
+	dbConn, err := sqlx.Connect(cfg.Db.Driver, cfg.Db.Url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect %w", err)
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     cfg.Redis.Address,
+		Password: cfg.Redis.Password,
+		DB:       cfg.Redis.DB,
+	})
+
+	if err := client.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to ping redis %w", err)
+	}
+
+	repos := repo.NewPeopleRepo(dbConn, client, cfg.Redis.Timeout)
+
+	return &Server{
+		logger:    logger,
+		repo:      repos,
+		router:    chi.NewRouter(),
+		doneChan:  make(chan struct{}),
+		closeChan: make(chan struct{}),
+	}, nil
+}
+
+func (s *Server) ListenAndServe() error {
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	s.logger.Info("shutting down")
+	close(s.closeChan)
+
+	for {
+		select {
+		case <-s.closeChan:
+			return nil
+		case <-ctx.Done():
+			return fmt.Errorf("context canceled: %w", ctx.Err())
+		}
+	}
+}
+
+func (s *Server) Close(ctx context.Context) error {
+	if err := s.repo.Close(ctx); err != nil {
+		return err
+	}
+	return nil
+}
