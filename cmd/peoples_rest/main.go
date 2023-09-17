972b0a05dc03b72840567a796b45ed8947857841
dmitrij <cwfrcpp@gmail.com>
2023-09-17 14:40:15 +0300

    updated main, created workable executable

--- cmd/peoples_rest/main.go
+++ cmd/peoples_rest/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"context"
-	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config"
-	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config/rest_config"
-	"github.com/Dmitrij-Kochetov/peoples/internal/application/presentation/rest"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config"
+	"github.com/Dmitrij-Kochetov/peoples/internal/adapter/config/rest_config"
+	"github.com/Dmitrij-Kochetov/peoples/internal/application/presentation/rest"
 )
 
 func main() {
@@ -38,6 +40,8 @@ func run(cfg rest_config.Config) (<-chan error, error) {
 		syscall.SIGQUIT,
 	)
 
+	httpServ := server.GetHttp()
+
 	go func() {
 		<-ctx.Done()
 		log.Println("Shutting down...")
@@ -56,6 +60,10 @@ func run(cfg rest_config.Config) (<-chan error, error) {
 			close(errChan)
 		}()
 
+		if err := httpServ.Shutdown(ctxTimeout); err != nil {
+			errChan <- err
+		}
+
 		if err := server.Shutdown(ctxTimeout); err != nil {
 			errChan <- err
 		}
@@ -66,9 +74,10 @@ func run(cfg rest_config.Config) (<-chan error, error) {
 	go func() {
 		log.Println("Starting server...")
 
-		if err := server.ListenAndServe(); err != nil {
+		if err := httpServ.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			errChan <- err
 		}
+
 	}()
 
 	return errChan, nil
